cf5370d85f8b2e1b0dcd56034ae15c9d39f3ce3a
Adwelz <43895468+Adwelz@users.noreply.github.com>
2023-04-20 12:42:29 +0200

    Segm

--- src/App.java
+++ src/App.java
@@ -6,23 +6,27 @@ public class App {
 
         long startTime = System.nanoTime();
 
-        ImageUtility imgUtil = new ImageUtility("src/resources/config.properties");
+        Individual individual = new Individual();
 
-        //System.out.println(imgUtil.dist(0, 241));
+        String[] geno = individual.getGenotype();
+        System.out.println(geno[0]+" "+ geno[1]+" "+geno[2]);
+        System.out.println(geno[0+ImageUtility.getInstance().width]+" "+ geno[1+ImageUtility.getInstance().width]+" "+geno[2+ImageUtility.getInstance().width]);
+        System.out.println(geno[0+ImageUtility.getInstance().width*2]+" "+ geno[1+ImageUtility.getInstance().width*2]+" "+geno[2+ImageUtility.getInstance().width*2]);
+        System.out.println(geno[0+ImageUtility.getInstance().width*3]+" "+ geno[1+ImageUtility.getInstance().width*3]+" "+geno[2+ImageUtility.getInstance().width*3]);
+        System.out.println(geno[0+ImageUtility.getInstance().width*4]+" "+ geno[1+ImageUtility.getInstance().width*4]+" "+geno[2+ImageUtility.getInstance().width*4]);
+        System.out.println(geno[0+ImageUtility.getInstance().width*5]+" "+ geno[1+ImageUtility.getInstance().width*5]+" "+geno[2+ImageUtility.getInstance().width*5]);
 
-
-        short[][] adjMatrix = imgUtil.genAdjMatrix();
-
-        System.out.println(Arrays.toString(imgUtil.prim(adjMatrix)));
-
-        //List<Edge> graph = imgUtil.genGraph();
-        
-        //List<Edge> mst = imgUtil.primsMST(graph, 0);
-
-        //String[] geno = imgUtil.mstToGeno(mst, 0);
-
-        //System.out.println(geno[0]);
-        System.out.println(241*161);
+        System.out.println(individual.getSegments().get(0).size());
+        //System.out.println(individual.getSegments()[0].length);
+        //System.out.println(Arrays.toString(individual.genotype2parent()));
+        /* int[] parent =individual.genotype2parent();
+        for(int i =0;i<parent.length;i++){
+            if(parent[i]==-1){
+                System.out.println("OUI");
+            }
+        } */
+        //System.out.println(Arrays.toString(individual.getGenotype()));
+        //System.out.println(Arrays.asList(geno));
 
         long endTime   = System.nanoTime();
 
--- src/ImageUtility.java
+++ src/ImageUtility.java
@@ -5,7 +5,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
 import java.util.Properties;
 
 import javax.imageio.ImageIO;
@@ -17,10 +16,12 @@ public class ImageUtility {
     BufferedImage img;
     int width;
     int height;
-    
-    public ImageUtility(String configPath) {
+
+    private static ImageUtility single_instance = null;
+
+    public ImageUtility() {
         try {
-            config.load(new FileInputStream(configPath));
+            config.load(new FileInputStream("src/resources/config.properties"));
 
             imgPath = config.getProperty("imgPath");
 
@@ -35,17 +36,25 @@ public class ImageUtility {
         }
     }
 
-    public int[] getRGB(int x, int y){
-        int rgb = img.getRGB(x,y);
-        
-        int red = (rgb >> 16) & 0xff;            
-        int green = (rgb >> 8) & 0xff;            
-        int blue = rgb & 0xff;
-
-        return new int[]{red, green, blue};
+    public static synchronized ImageUtility getInstance()
+    {
+        if (single_instance == null)
+            single_instance = new ImageUtility();
+            
+        return single_instance;
     }
 
-    public double dist(int i, int j){
+    public int[] getRGB(int x, int y) {
+        int rgb = img.getRGB(x, y);
+
+        int red = (rgb >> 16) & 0xff;
+        int green = (rgb >> 8) & 0xff;
+        int blue = rgb & 0xff;
+
+        return new int[] { red, green, blue };
+    }
+
+    public double dist(int i, int j) {
         int xi = i % this.width;
         int yi = i / this.width;
 
@@ -59,77 +68,74 @@ public class ImageUtility {
         int dG = RGBi[1] - RGBj[1];
         int dB = RGBi[2] - RGBj[2];
 
-        return Math.sqrt( Math.pow(dR, 2) + Math.pow(dG, 2) + Math.pow(dB, 2));
+        return Math.sqrt(Math.pow(dR, 2) + Math.pow(dG, 2) + Math.pow(dB, 2));
     }
 
-    public short[][] genAdjMatrix(){
+    public short[][] genAdjMatrix() {
         int n = this.width * this.height;
 
         short[][] graph = new short[n][n];
 
-        for(int i=0;i<n;i++){
-            if(i-1>=0){
-                short weight = (short) dist(i, i-1);
+        for (int i = 0; i < n; i++) {
+            if (i - 1 >= 0) {
+                short weight = (short) dist(i, i - 1);
 
-                graph[i][i-1] = weight;
-                graph[i-1][i] = weight;
+                graph[i][i - 1] = weight;
+                graph[i - 1][i] = weight;
             }
-            if(i+1<n){
-                short weight = (short) dist(i, i+1);
+            if (i + 1 < n) {
+                short weight = (short) dist(i, i + 1);
 
-                graph[i][i+1] = weight;
-                graph[i+1][i] = weight;
-            } 
-            if(i-this.width>=0){
-                short weight = (short) dist(i, i-this.width);
-
-                graph[i][i-this.width] = weight;
-                graph[i-this.width][i] = weight;
+                graph[i][i + 1] = weight;
+                graph[i + 1][i] = weight;
             }
-            if(i+this.width<n){
-                short weight = (short) dist(i, i+this.width);
+            if (i - this.width >= 0) {
+                short weight = (short) dist(i, i - this.width);
 
-                graph[i][i+this.width] = weight;
-                graph[i+this.width][i] = weight;
+                graph[i][i - this.width] = weight;
+                graph[i - this.width][i] = weight;
+            }
+            if (i + this.width < n) {
+                short weight = (short) dist(i, i + this.width);
+
+                graph[i][i + this.width] = weight;
+                graph[i + this.width][i] = weight;
             }
         }
 
         return graph;
-    } 
+    }
 
-    public int[] prim(short[][] graph) {
+    public int[] prim(short[][] graph, int firstVertice) {
         int n = graph.length;
         int[] parent = new int[n];
         short[] key = new short[n];
         boolean[] mstSet = new boolean[n];
-        
-        // Initialisation de key avec des valeurs infinies
+
         for (int i = 0; i < n; i++) {
             key[i] = Short.MAX_VALUE;
         }
-        
-        // Le sommet 0 est choisi comme point de départ
-        key[0] = 0;
-        parent[0] = -1;
-        
-        // Parcours des sommets pour construire l'arbre couvrant de poids minimal
-        for (int i = 0; i < n-1; i++) {
+
+        key[firstVertice] = 0;
+        parent[firstVertice] = -1;
+
+        for (int i = 0; i < n - 1; i++) {
             int u = minKey(key, mstSet);
             mstSet[u] = true;
-            
+
             List<Integer> vList = new ArrayList<>();
 
-            if(u>0){
-                vList.add(u-1);
+            if (u > 0) {
+                vList.add(u - 1);
             }
-            if(u>= this.width){
-                vList.add(u-this.width);
+            if (u >= this.width) {
+                vList.add(u - this.width);
             }
-            if(u<n-1){
-                vList.add(u+1);
+            if (u < n - 1) {
+                vList.add(u + 1);
             }
-            if(u<n-this.width){
-                vList.add(u+this.width);
+            if (u < n - this.width) {
+                vList.add(u + this.width);
             }
 
             for (int v : vList) {
@@ -139,10 +145,10 @@ public class ImageUtility {
                 }
             }
         }
-        
+
         return parent;
     }
-    
+
     public static int minKey(short[] key, boolean[] mstSet) {
         short min = Short.MAX_VALUE;
         int minIndex = -1;
@@ -155,125 +161,33 @@ public class ImageUtility {
         return minIndex;
     }
 
-    public List<Edge> genGraph(){
-        int n = this.width * this.height;
+    public String[] parent2genotype(int[] parent) {
+        int n = parent.length;
+        String[] genotype = new String[n];
+        for (int i = 0; i < n; i++) {
+            if (parent[i] == -1) {
+                genotype[i] = "none";
+            } else {
+                int x = parent[i];
+                if (x == i - 1) {
+                    genotype[i] = "left";
+                } else if (x == i + 1) {
+                    genotype[i] = "right";
+                } else {
+                    if (x == i - width) {
+                        genotype[i] = "up";
 
-        List<Edge> graph = new ArrayList<>();
-
-        for(int i=0;i<n;i++){
-            if(i-1>=0){
-                graph.add(new Edge(i, i-1, (short) dist(i, i-1)));
-            }
-            if(i+1<n){
-                graph.add(new Edge(i, i+1, (short) dist(i, i+1)));
-            } 
-            if(i-this.width>=0){
-                graph.add(new Edge(i, i-this.width, (short) dist(i, i-this.width)));
-            }
-            if(i+this.width<n){
-                graph.add(new Edge(i, i+this.width, (short) dist(i, i+this.width)));
-            }
-        }
-
-        return graph;
-    } 
-
-    public List<Edge> primsMST(List<Edge> edges, int firstVertice) {
-        int numVertices = this.height * this.width;
-
-        List<Edge> mst = new ArrayList<>();
-        boolean[] visited = new boolean[numVertices];
-        PriorityQueue<Edge> pq = new PriorityQueue<>();
-        
-        visited[firstVertice] = true;
-        for (Edge e : edges) {
-            if (e.u == firstVertice || e.v == firstVertice) {
-                pq.add(e);
-            }
-        }
-        
-        while (!pq.isEmpty()) {
-            Edge e = pq.poll();
-            if (visited[e.u] && visited[e.v]) {
-                continue;
-            }
-            mst.add(e);
-            if (!visited[e.u]) {
-                visited[e.u] = true;
-                for (Edge neighbor : edges) {
-                    if (neighbor.u == e.u || neighbor.v == e.u) {
-                        pq.add(neighbor);
-                    }
-                }
-            }
-            if (!visited[e.v]) {
-                visited[e.v] = true;
-                for (Edge neighbor : edges) {
-                    if (neighbor.u == e.v || neighbor.v == e.v) {
-                        pq.add(neighbor);
+                    } else {
+                        genotype[i] = "down";
                     }
                 }
             }
         }
-        
-        return mst;
+        return genotype;
     }
 
-    public static boolean containsFalse(boolean[] arr) {
-        for (boolean b : arr) {
-            if (!b) {
-                return true;
-            }
-        }
-        return false;
+    public int getWidth() {
+        return width;
     }
 
-    public String[] mstToGeno(List<Edge> edges, int firstVertice) {
-        int numVertices = this.height * this.width;
-
-        String[] geno = new String[numVertices];
-
-        boolean[] visited = new boolean[numVertices];
-
-        int currentVertice = firstVertice;
-
-        while(containsFalse(visited)){
-            visited[currentVertice] = true;
-
-            int nextVertice = -1;
-
-            for (Edge edge : edges) {
-                if (edge.u == currentVertice ) {
-                    nextVertice = edge.v;
-                    break;
-                }
-                if (edge.v == currentVertice ) {
-                    nextVertice = edge.u;
-                    break;
-                }
-            }
-
-            if(nextVertice == -1){
-                geno[currentVertice] = "none";
-                break;
-            }
-
-            if(nextVertice == currentVertice+1){
-                geno[currentVertice] = "right";
-            }
-            if(nextVertice == currentVertice-1){
-                geno[currentVertice] = "left";
-            }
-            if(nextVertice == currentVertice+this.width){
-                geno[currentVertice] = "down";
-            }
-            if(nextVertice == currentVertice-this.width){
-                geno[currentVertice] = "up";
-            }
-
-        }
-
-        return geno;
-        
-    }
 }
